d7ad29aff1d3330abce9ad6ebc60bdd7f073f4dc
gimziae <gimziae@gmail.com>
2022-01-20 14:01:52 +0900

    faq js추가

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const faq = document.querySelector("#faq");
 const lis = faq.querySelectorAll("li");
-console.log(lis);
+const enableClick = true;
 
 lis.forEach((li,index)=>{
     li.addEventListener("click", e=>{
         e.preventDefault();
 
-        for(let i=0; i<lis.length; i++){
-            lis[i].classList.remove("on");
+        if(li.classList.contains("on")){
+            lis[index].classList.remove("on");
 
+        }else{
+            lis[index].classList.add("on");
         }
-        lis[index].classList.add("on");
-    })
+    })  
 })
+
+
