5bca50244ae976a90ed7a5697468f5f5261edfb7
gimziae <gimziae@gmail.com>
2022-01-23 04:48:55 +0900

    tablet menuMo 창열기/ 닫기 js 추가

--- js/main.js
+++ js/main.js
@@ -21,3 +21,20 @@
 //     })
 // })
 
+const body = document.querySelector("body");
+const header = document.querySelector("#header")
+const btnCall = header.querySelector(".btnCall");
+const menuMo = header.querySelector(".menuMo");
+console.log(menuMo);
+
+btnCall.addEventListener("click", e=>{
+    e.preventDefault();
+
+    btnCall.classList.toggle("on");
+    menuMo.classList.toggle("on");
+    
+    if(btnCall.classList.contains("on")){
+        body.style.opacity = "0.6";
+    }else body.style.opacity = "1";
+    
+})
