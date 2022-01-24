0074641ce5a1265ce2add5dff0e061a5d6a90d60
gimziae <gimziae@gmail.com>
2022-01-25 03:51:28 +0900

    about(department) 페이지 추가

---
+++ js/member.js
@@ -0,0 +1,25 @@
+const frame = document.querySelector(".member");
+
+console.log(frame);
+
+fetch("data/member.json")
+.then(data=>{
+    return data.json();
+    
+})
+.then(json => {
+
+    const memberInfo = json.data;
+    
+    let tags = '';
+    memberInfo.map(member=>{
+        tags += `
+                <article>
+                    <img src="${member.pic}">
+                    <h2>${member.name}</h2>
+                    <p>${member.position}</p>
+                </article>
+                 `;
+    })
+    frame.innerHTML = tags;
+})
