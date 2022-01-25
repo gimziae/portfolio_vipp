1d7523ff02024f2635bc0bf9e09744a072c04670
gimziae <gimziae@gmail.com>
2022-01-26 01:53:59 +0900

    brand 페이지 레이아웃 & 모바일 메뉴 수정

--- js/youtube.js
+++ js/youtube.js
@@ -5,46 +5,77 @@ http://www.googleaips.com/youtube/v3/playlistItems
 */ 
 
 const vidList = document.querySelector(".vidList");
-const key = 'AIzaSyBLmcoTvbceDJAzuKFsH7ks2aR4MwwJqn4';
+const key = "AIzaSyBLmcoTvbceDJAzuKFsH7ks2aR4MwwJqn4";
 const playlistId = "PLOK2vR6uL5S2AEhapjLmpYE2WIfe_9HWR";
 const num = 10;
 const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`; 
 
 fetch(url)
-.then(data=>{ 
-    return data.json(); 
+.then(data=>{
+
+    return data.json();
+    
 })
 .then(json=>{
+   
     let items = json.items;
     console.log(items);
 
-    let result = '';
+    let result = "";
 
     items.map(item=>{
+
+        let title = item.snippet.title;
+        if(title.length > 40){
+            title = title.substr(0, 40)+"...";
+        }
+
+        let description = item.snippet.description;
+        if(description.length > 100){
+            description = description.substr(0, 100)+"...";
+        }
+
         result += `
-                    <article>
-                        <a href="${item.snippet.resourceId.videoId}" class="pic">
-                            <img class="thumb" src="${item.snippet.thumbnails.medium.url}" alt="">
-                        </a>
-                        <div class="con">
-                            <h2>${item.snippet.title}</h2>
-                        </div>
-                    </article>                   
-                  `;
-    })
+                <article>
+                    <a href="${item.snippet.resourceId.videoId}" class="pic">
+                        <img src="${item.snippet.thumbnails.medium.url}" class="thumb">        
+                    </a>
+                    <div class="con">
+                        <h2>${title}</h2>
+                        <span></span
+                    </div>
+                    
+                </article>
+                `;
+    });
 
     vidList.innerHTML = result;
+
 })
 
-vidList.addEventListener("click", ()=>{
-    e.preventDefault();
+vidList.addEventListener("click", e=>{
+    e.preventDefault(); 
 
-    const vidId = e.target.closest("a").getAttribute("href");
+    const vidId = e.target.closest("article").querySelector("a").getAttribute("href"); 
+
     let pop = document.createElement("figure");
-    pop.classList.add("pop");
+    pop.classList.add("pop"); 
     pop.innerHTML = `
-                    <iframe width="560" height="315" src="${vidId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <span class=btnClose>CLOSE</span>
-                    `;
-    vidList.append(pop);
+                    <iframe src="https://www.youtube.com/embed/${vidId}" frameborder="0" allowfullscreen class="vidPlay"></iframe>
+                    <span class="btnClose">close</span>
+                    `; 
+
+    vidList.append(pop); 
+})
+
+vidList.addEventListener("click", e=>{
+    const pop = vidList.querySelector("figure");
+
+    if(pop !=null){
+        const close = pop.querySelector("span");
+
+        if(e.target = close){
+            e.target.closest("figure").remove();
+        }
+    }
 })
