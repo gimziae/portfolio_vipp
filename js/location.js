f12b36b766e290eef2226e1da407abfe38970d61
gimziae <gimziae@gmail.com>
2022-02-04 03:38:27 +0900

    location 반응형

--- js/location.js
+++ js/location.js
@@ -5,7 +5,9 @@
 var mapContainer = document.getElementById('map'); // 지도를 표시할 div 
 
 const branch_btns = document.querySelectorAll(".branch li");
+const branch_info = document.querySelectorAll("#branchInfo article");
 
+console.log(branch_info);
 let zoom = true; //확대 축소 가능
 let drag = false; // 드래그 가능
 
@@ -24,25 +26,28 @@ var markerOptions =[
         title: "Main",
         latlng: new kakao.maps.LatLng(37.5120725,127.057503),
         imgSrc: 'img/marker.png',
-        imgSize: new kakao.maps.Size(120, 120),
+        imgSize: new kakao.maps.Size(100, 100),
         imgPos: {offset: new kakao.maps.Point(116, 69)},
-        button: branch_btns[0]
+        button: branch_btns[0],
+        info: branch_info[0]
     },
     {
         title: "Branch1",
         latlng: new kakao.maps.LatLng(37.266444,126.9972137),
         imgSrc: 'img/marker.png',
-        imgSize: new kakao.maps.Size(120, 120),
+        imgSize: new kakao.maps.Size(100, 100),
         imgPos: {offset: new kakao.maps.Point(116, 69)},
-        button: branch_btns[1]
+        button: branch_btns[1],
+        info: branch_info[1]
     },
     {
         title: "Branch2",
         latlng: new kakao.maps.LatLng(37.5243081,126.9522836),
         imgSrc: 'img/marker.png',
-        imgSize: new kakao.maps.Size(120, 120),
+        imgSize: new kakao.maps.Size(100, 100),
         imgPos: {offset: new kakao.maps.Point(116, 69)},
-        button: branch_btns[2]
+        button: branch_btns[2],
+        info: branch_info[2]
     }
 ];
 
@@ -60,8 +65,10 @@ for(let i=0; i<markerOptions.length; i++){
 
         for(let k=0; k<markerOptions.length; k++){
             markerOptions[k].button.classList.remove("on");
+            markerOptions[k].info.classList.remove("on");
         }
         markerOptions[i].button.classList.add("on");
+        markerOptions[i].info.classList.add("on");
 
         moveTo(markerOptions[i].latlng);
     }
