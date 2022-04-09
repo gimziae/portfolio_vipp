0eb7f8b67ab854251169a3d6cc28376b7f54d4e8
gimziae <gimziae@gmail.com>
2022-04-09 15:20:33 +0900

    mainpage 변경항목(#intro, #rooms) 반응형 적용 완료

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const joinWeb = header.querySelector(".menuWeb .joinWeb");
 const logIn = joinWeb.querySelector(".login");
 const logPop = joinWeb.querySelector(".popup");
 
-const introTxt = document.querySelector("#intro>.inner");
+const introTxt = document.querySelector("#intro .inner");
 console.log(logPop);
 console.log(logIn);
 console.log(introTxt);
@@ -28,30 +28,3 @@ logIn.addEventListener("mouseenter", e=>{
 // window.addEventListener("load", ()=>{
 //     introTxt.classList.add("on");
 // })
-
-// const intro = body.querySelector("#intro");
-// const slides = intro.querySelectorAll(".swiper-slide img");
-
-// const strong = intro.querySelector("strong");
-// const span = intro.querySelector(".pagenum span");
-// const total = slides.length - 2;
-// console.log(total);
-
-// span.innerText = total;
-
-// for(let i=0; i<total; i++){
-//     lis[i].addEventListener("click", e=>{
-//         e.preventDefault();
-
-//         let clickA = lis[i].querySelector("a");
-//         let imgSrc = clickA.getAttribute("href");
-//         console.log(imgSrc);
-//         backImg.setAttribute("src", imgSrc);
-
-//         for(let el of lis){
-//             el.classList.remove("on");
-//         }
-//         lis[i].classList.add('on');
-//         strong.innerText = i+1;
-//     })
-// }
